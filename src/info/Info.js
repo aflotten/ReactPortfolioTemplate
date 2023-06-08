824b7c6c0c77d32b915d6e414a8151828b1cb33e
aflotten <anders3835@hotmail.com>
2023-06-07 21:08:39 -0600

    some touch ups - need to complete projects section

--- src/info/Info.js
+++ src/info/Info.js
@@ -23,84 +23,80 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Anders",
+    lastName: "Flotten",
+    initials: "AF", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
+            emoji: '🌞',
+            text: 'powered by the sun'
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'located in Calgary'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "looking for work!"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "andersflotten97@gmail.com"
         }
     ],
     socials: [
+        
+    
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/aflotten",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/anders-flotten-311246272/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
         {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
+            link: "https://resume.creddle.io/resume/gos172qj6pu",
+            icon: "fa fa-file",
+            label: 'resume'
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hi there! I'm Anders. I am a recent graduate from Lighthouse Labs' coding bootcamp and looking to get into the tech industry. I have pivoted from a background in the sciences where I recieved a bachelors degree in Biology. My hobbies include hanging out with my puppy Bruce, weight lifting, and video games! Please feel free to reach out to me at anytime!",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html', 'CSS', 'postgreSQL', 'nodeJS'],
+            exposedTo: ['nextJS', 'ruby on rails', 'prisma']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'video games',
+            emoji: '🎮'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'weight lifting',
+            emoji: '🏋️'
+        },
+        {
+            label: 'golf',
+            emoji: '🏌️'
         },
         {
             label: 'movies',
             emoji: '🎥'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'BRUCEY BOY',
+            emoji: '🦴'
         }
 // Same as above, change the emojis to match / relate to your hobbies or interests.
 // You can also remove the emojis if you'd like, I just think they look cute :P
